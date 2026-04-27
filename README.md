9fa5b8080095c06b9838b04ed326a5ab760d9b9f
Dylan <meharu.dylan@gmail.com>
2026-04-27 11:32:20 -0300

    Resolucion ejercicios

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TP1 - Recursividad
 
-Resolución de los ejercicios 5 al 22 en Python usando recursividad.
+Resolución de los ejercicios 5 y 22 en Python usando recursividad.
--- TP1.py
+++ TP1.py
@@ -13,124 +13,23 @@ def romano_a_decimal(s):
 
     return rec(0)
 
-#Ejercicio 6
-def invertir(cadena):
-    if cadena == "":
-        return ""
-    return invertir(cadena[1:]) + cadena[0]
 
-#Ejercicio 7
-def serie(n):
-    if n == 1:
-        return 1
-    return 1/n + serie(n-1)
-
-#Ejercicio 8
-def decimal_a_binario(n):
-    if n < 2:
-        return str(n)
-    return decimal_a_binario(n//2) + str(n%2)
-
-#Ejercicio 9
-def log_entero(n, b):
-    if n < b:
-        return 0
-    return 1 + log_entero(n//b, b)
-
-#Ejercicio 10    
-def contar_digitos(n):
-    if n < 10:
-        return 1
-    return 1 + contar_digitos(n//10)
-
-#Ejercicio 11    
-def invertir_num(n, res=0):
-    if n == 0:
-        return res
-    return invertir_num(n//10, res*10 + n%10)
-                     
-#Ejercicio 12
-def mcd(a, b):
-    if b == 0:
-        return a
-    return mcd(b, a % b)
-
-#Ejercicio 13
-# Versión recursiva
-def mcm(a, b):
-    if a == b:
-        return a
-    if a > b:
-        return mcm(a - b, b)
-    return mcm(a, b - a)
-
-#Ejercicio 14
-def suma_digitos(n):
-    if n == 0:
-        return 0
-    return n % 10 + suma_digitos(n//10)
-
-#Ejercicio 15
-def raiz_entera(n, i=0):
-    if i*i > n:
-        return i-1
-    return raiz_entera(n, i+1)
-
-#Ejercicio 16
-def sucesion_geo(a1, r, n):
-    if n == 1:
-        return a1
-    return r * sucesion_geo(a1, r, n-1)
-
-#Ejercicio 17
-def mostrar_reves(vec, i):
-    if i < 0:
-        return
-    print(vec[i])
-    mostrar_reves(vec, i-1)
-
-#Ejercicio 18
-def recorrer_matriz(mat, i=0, j=0):
-    if i >= len(mat):
-        return
-    if j >= len(mat[0]):
-        recorrer_matriz(mat, i+1, 0)
-        return
-    print(mat[i][j])
-    recorrer_matriz(mat, i, j+1)
-
-#Ejercicio 19
-
-def f(n):
-    if n == 1:
-        return 2
-    return n + 1 / f(n-1)
-
-#Ejercicio 20
-def busqueda(lista, x, i=0):
-    if i >= len(lista):
-        return False
-    if lista[i] == x:
-        return True
-    return busqueda(lista, x, i+1)
-
-#Ejercicio 21
-def busqueda_binaria(lista, x, ini, fin):
-    if ini > fin:
-        return False
-    mid = (ini + fin) // 2
-    if lista[mid] == x:
-        return True
-    elif x < lista[mid]:
-        return busqueda_binaria(lista, x, ini, mid-1)
-    else:
-        return busqueda_binaria(lista, x, mid+1, fin)
-    
-#Ejercicio 22
+#Ejercicio 22    
 def usar_la_fuerza(mochila, i=0):
+    # Caso base: se vació la mochila, no había sable
     if i >= len(mochila):
-        return False, -1  # -1 indica "no encontrado"
+        print(f"Se revisaron todos los objetos. No hay sable de luz.")
+        return False, 0
+
+    # Simulamos "sacar" el objeto actual
+    print(f"Sacando objeto {i+1}: {mochila[i]}")
+
+    # Caso base: encontramos el sable
     if mochila[i] == "sable de luz":
-        return True, i    
+        print(f"¡Encontrado el sable de luz!")
+        return True, i+1   # i+1 = cantidad de objetos que se sacaron
+
+    # Caso recursivo: seguimos buscando
     return usar_la_fuerza(mochila, i+1)
+
 
